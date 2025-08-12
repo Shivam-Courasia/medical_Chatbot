fc51e46a339aa0f2ad1c14bee21df55f4bcbdea1
shivam chourasia <shivamchourasia2003@gmail.com>
2025-08-12 16:10:38 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,17 @@
+# Directories
+mkdir -p src
+mkdir -p research
+
+
+#create file
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env 
+touch setup.py
+touch app.py
+touch research/trials.py
+touch requirements.txt
+
+
+echo "Directories and files created successfully!."
